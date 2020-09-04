aac6f987b71f15e4b41d9bc75258425d1ed79b40
gurjotsaini <gurjotsaini1992@gmail.com>
2020-09-04 14:06:54 +0530

    Initial release

---
+++ app/Http/Controllers/User/MeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+
+class MeController extends Controller
+{
+    /**
+     * Return current authenticated user
+     *
+     * @return UserResource|JsonResponse
+     */
+    public function getMe() {
+        if (auth()->check()) {
+            $user = auth()->user();
+            return new UserResource($user);
+        }
+
+        return response()->json(null, 200);
+    }
+}
