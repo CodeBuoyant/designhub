b1887633c0520fc7acfb0b2aa29f5693df31fd39
gurjotsaini <gurjotsaini1992@gmail.com>
2020-09-07 23:56:17 +0530

    Added: criterias for repositories

--- app/Http/Controllers/Designs/DesignController.php
+++ app/Http/Controllers/Designs/DesignController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\DesignResource;
 use App\Models\Design;
 use App\Repositories\Contracts\IDesign;
+use App\Repositories\Eloquent\Criteria\ForUser;
+use App\Repositories\Eloquent\Criteria\IsLive;
+use App\Repositories\Eloquent\Criteria\LatestFirst;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -19,7 +22,11 @@ class DesignController extends Controller
     }
 
     public function index() {
-        $designs = $this->designs->all();
+        $designs = $this->designs->withCriteria([
+            new LatestFirst(),
+            new IsLive(),
+            new ForUser(1)
+        ])->all();
         return DesignResource::collection($designs);
     }
 
--- app/Repositories/Eloquent/DesignRepository.php
+++ app/Repositories/Eloquent/DesignRepository.php
@@ -11,6 +11,14 @@ class DesignRepository extends BaseRepository implements IDesign
         return Design::class;
     }
 
+    public function allLive() {
+        return $this->model->where('is_live', true)->get();
+    }
+
+    public function allLiveLatestFirst() {
+        return $this->model->where('is_live', true)->latest();
+    }
+
     public function applyTags($id, array $data) {
         $design = $this->find($id);
         $design->retag($data);
