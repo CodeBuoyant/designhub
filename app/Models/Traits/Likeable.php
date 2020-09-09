26efb292239bea2e008153d54e9599188db92bbc
gurjotsaini <gurjotsaini1992@gmail.com>
2020-09-10 00:01:33 +0530

    Added: likes functionality pt 1

--- app/Models/Traits/Likeable.php
+++ app/Models/Traits/Likeable.php
@@ -6,22 +6,42 @@ use App\Models\Like;
 
 trait Likeable
 {
+    // Model observer: run delete method if the model is deleted
+    public static function bootLikable() {
+        static::deleting(function($model){
+            $model->removeLikes();
+        });
+    }
+
+    // Delete likes when model is being deleted
+    public function removeLikes() {
+        if($this->likes()->count()){
+            $this->likes()->delete();
+        }
+    }
+
     public function likes() {
         return $this->morphMany(Like::class, 'likeable');
     }
 
     public function like() {
         // Check if the person is authenticated
-        if ( !auth()->check() ) {
-            return;
-        }
+        if ( !auth()->check() ) return;
 
         // Check if the current user already liked
-        if ($this->isLikedByUser(auth()->id())) {
-            return;
-        }
+        if ($this->isLikedByUser(auth()->id())) return;
 
         $this->likes()->create([ 'user_id' => auth()->id() ]);
+    }
+
+    public function unlike() {
+        // Check if the person is authenticated
+        if ( !auth()->check() ) return;
+
+        // Check if the current user already unliked
+        if ( ! $this->isLikedByUser(auth()->id())) return;
+
+        $this->likes()->where([ 'user_id' => auth()->id() ])->delete();
     }
 
     public function isLikedByUser( $userId ) {
