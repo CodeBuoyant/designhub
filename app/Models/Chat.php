963d59015526bf0f3c4a9e16bbe932402248f72f
gurjotsaini <gurjotsaini1992@gmail.com>
2020-09-25 20:30:12 +0530

    Added: comments in some models

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,25 +4,41 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chat extends Model
 {
+    /**
+     * @return BelongsToMany
+     */
     public function participants() {
         return $this->belongsToMany(User::class, 'participants');
     }
 
+    /**
+     * @return HasMany
+     */
     public function messages() {
         return $this->hasMany(Message::class);
     }
 
     // Helper methods
-
-    // Get latest message
+    /**
+     * Get latest message
+     *
+     * @return Model|HasMany|object|null
+     */
     public function getLatestMessageAttribute() {
         return $this->messages()->latest()->first();
     }
 
-    // Check if chat is unread
+    /**
+     * Check if chat is unread
+     *
+     * @param $userId
+     * @return bool
+     */
     public function isUnreadForUser($userId) {
         return (bool)$this->messages()
             ->whereNull('last_read')
@@ -30,6 +46,9 @@ class Chat extends Model
             ->count();
     }
 
+    /**
+     * @param $userId
+     */
     public function markAsReadForUser($userId) {
         $this->messages()
             ->whereNull('last_read')
